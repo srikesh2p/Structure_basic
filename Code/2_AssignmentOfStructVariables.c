c8f8e09533269880b249706ca59f329aa7bcc30e
srikesh <srikesh.p108@gmail.com>
2019-01-27 22:38:06 +0530

    Remove Line from Code

--- Code/2_AssignmentOfStructVariables.c
+++ Code/2_AssignmentOfStructVariables.c
@@ -3,26 +3,17 @@
 #include<stdio.h>
 
 struct student {
-
 	char name[20];
-
 	int rollno;
-
 	float marks;
-
 };
 
-main( )
+int main( )
 
 {
-
 	struct student stu1 = {"Anjali", 12, 98},stu2;
-
 	stu2 = stu1;
-
 	printf("stu1 : %s   %d   %.2f\n", stu1.name, stu1.rollno, stu1.marks);
-
 	printf("stu2 : %s   %d   %.2f\n", stu2.name, stu2.rollno, stu2.marks);
-
 }
 
--- Code/3_ArrayOfStrucutre.c
+++ Code/3_ArrayOfStrucutre.c
@@ -3,11 +3,8 @@
 #include<stdio.h>
 
 struct student {
-
 	char name[20];
-
 	int rollno;
-
 	float marks;
 
 };
@@ -15,23 +12,15 @@ struct student {
 main( )
 
 {
-
 	int i;
-
 	struct student stuarr[10];
-
 	for(i=0; i<10; i++)
-
 	{
-
 		printf("Enter name, rollno and marks  : "); 
-
 		scanf("%s%d%f", stuarr[i].name, &stuarr[i].rollno, &stuarr[i].marks);
-
 	}
 
 	for(i=0; i<10; i++)
-
 		printf("%s  %d   %f \n", stuarr[i].name, stuarr[i].rollno, stuarr[i].marks);
 
 }
